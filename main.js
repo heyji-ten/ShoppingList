3412ca00ec1196797f393d1219ddeb221cbc10e5
heyji-ten <59608643+heyji-ten@users.noreply.github.com>
2021-06-14 15:25:17 +0900

    JS: comment delete

--- main.js
+++ main.js
@@ -3,22 +3,14 @@ const memo = document.querySelector('.shopping__memo');
 const addBtn = document.querySelector('.addbtn');
 
 function onAdd() {
-    // 1. 사용자가 입력한 텍스트를 받아옴
     const text = memo.value;
     if(text === ''){
         memo.focus();
         return;
     }
-    // 2. 새로운 아이템을 만듬 (텍스트 + 삭제버튼)
     const item = createItem();
-
-    // 3. list__item 컨테이너 안에 새로 만든 아이템을 추가
     items.appendChild(item);
-
-    // 4. 새로 추가된 아이템으로 스크롤링
     item.scrollIntoView({block: 'center'});
-
-    // 5. 인풋을 초기화
     memo.value = '';
     memo.focus();
 }
